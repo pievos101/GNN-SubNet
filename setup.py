8942efbd0718bf388ec5f339f19b040261d7d6b4
Marcus Bloice <marcus.bloice@medunigraz.at>
2022-05-05 15:05:40 +0200

    Changes to setup.py for package release.

--- setup.py
+++ setup.py
@@ -3,13 +3,15 @@ from setuptools import setup
 setup(
     name='GNNSubNet',
     packages=['GNNSubNet'],
-    version='0.2.10',
-    description='Image augmentation library for Machine Learning',
-    long_description='Image augmentation library for Machine Learning',
+    version='0.1.0',
+    author=" Bastian Pfeifer",
+    author_email="damien.j.martin@gmail.com",
+    description='Disease Subnetwork Detection with Explainable Graph Neural Networks',
+    long_description='Disease Subnetwork Detection with Explainable Graph Neural Networks',
     license='MIT',
-    url='https://github.com/mdbloice/Augmentor',                            # URL to GitHub repo
+    url='https://github.com/pievos101/GNN-SubNet',                            # URL to GitHub repo
     # download_url='https://github.com/mdbloice/Augmentor/tarball/0.1.1',   # Get this using git tag
-    keywords=['image', 'augmentation', 'artificial', 'generation', 'machine', 'learning'],
+    keywords=['graph-neural-networks', 'disease-networks', 'explainable-ai'],
     include_package_data=True,  # This will include all files in MANIFEST.in in the package when installing.
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -18,22 +20,20 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     install_requires=[
-        'Pillow>=5.2.0',
+        'torch>=1.11.0',
         'tqdm>=4.9.0',
-        #'future>=0.16.0',
-        'numpy>=1.11.0'
-        'futures>=3.2.0; python_version == "2.7"'
-    ]
-    # zip_safe=False # Check this later.
+        'numpy>=1.22.3'
+    ],
+    include_package_data=True,
+    package_data={'': ['datasets/Synthetic/*.txt']}
 )
