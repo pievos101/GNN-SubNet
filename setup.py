328c805bc513e061609f965b2634b9008d1d30fe
Marcus Bloice <marcus.bloice@medunigraz.at>
2022-05-05 15:37:50 +0200

    Changes to setup.py

--- setup.py
+++ setup.py
@@ -9,15 +9,13 @@ setup(
     description='Disease Subnetwork Detection with Explainable Graph Neural Networks',
     long_description='Disease Subnetwork Detection with Explainable Graph Neural Networks',
     license='MIT',
-    url='https://github.com/pievos101/GNN-SubNet',                            # URL to GitHub repo
-    # download_url='https://github.com/mdbloice/Augmentor/tarball/0.1.1',   # Get this using git tag
+    url='https://github.com/pievos101/GNN-SubNet',
     keywords=['graph-neural-networks', 'disease-networks', 'explainable-ai'],
-    include_package_data=True,  # This will include all files in MANIFEST.in in the package when installing.
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Natural Language :: English',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: GPL-3.0',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
@@ -35,5 +33,5 @@ setup(
         'numpy>=1.22.3'
     ],
     include_package_data=True,
-    package_data={'': ['datasets/Synthetic/*.txt']}
+    package_data={'': ['datasets/synthetic/*.txt']}
 )
