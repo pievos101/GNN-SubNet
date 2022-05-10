50446ed0b58b6c86446578b681d7db6e862aed02
Marcus Bloice <marcus.bloice@medunigraz.at>
2022-05-10 11:37:10 +0200

    Added logo placegolder to conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -79,6 +79,7 @@ pygments_style = 'sphinx'
 #
 # html_theme = 'alabaster'
 html_theme = 'default'
+#html_logo = 'logo.png'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
